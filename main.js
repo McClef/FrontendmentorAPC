a1856b6b6388d8ba7a39aca83bbbfef9f9c2bdf3
Martins Michael <clefmaj@gmail.com>
2020-07-14 13:12:21 +0100

    Airticle Preview

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggle(ele) {
+    var pop = document.getElementById('pop');
+    if (pop.style.display == 'block') {
+        pop.style.display = 'none';
+    }
+    else {
+        pop.style.display = 'block';
+    }
+}
